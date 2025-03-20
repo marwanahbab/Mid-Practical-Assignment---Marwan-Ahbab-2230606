1c5fc85f5faa5daf20534ce546f71551c4feabc7
marwanahbab <marwanaeltertius@gmail.com>
2025-03-20 18:31:45 +0600

    changed javascript code to properly validate text being inputted or not

--- contact.js
+++ contact.js
@@ -1,3 +1,15 @@
 function start() {
-    alert("Message Sent")
+    
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+   
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill out all the text boxes before sending the message.");
+        return false; 
+    } else {
+        alert("Message Sent!");
+        return true; 
+    }
 }
